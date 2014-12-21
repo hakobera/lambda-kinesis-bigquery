c2f9232d90116c814e0e7cbe304e7fc7c5939bd8
Kazuyuki Honda <hakobera@gmail.com>
2014-12-21 15:52:19 +0900

    First implementation

---
+++ scripts/upload_function.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+echo "==============="
+echo "Install npm package for production"
+rm -rf ./node_modules
+npm install --production
+
+echo "==============="
+echo "Create working folder"
+rm -rf ./tmp
+mkdir ./tmp
+
+echo "==============="
+echo "Create package"
+zip tmp/KinesisBigQuery.zip KinesisBigQuery.js
+zip tmp/KinesisBigQuery.zip package.json
+zip tmp/KinesisBigQuery.zip gcpconfig.json
+zip tmp/KinesisBigQuery.zip gcpkey.json
+zip -r tmp/KinesisBigQuery.zip node_modules/
+
+echo "==============="
+echo "Upload function"
+aws lambda upload-function \
+   --function-name KinesisBigQuery \
+   --function-zip tmp/KinesisBigQuery.zip \
+   --role $IAM_ROLE \
+   --mode event \
+   --handler KinesisBigQuery.handler \
+   --runtime nodejs \
+   --debug
+
+echo "==============="
+echo "Done"
