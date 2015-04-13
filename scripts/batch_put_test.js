cb5648aa6af033f93fdf8d62044670f58a0fa8ef
Kazuyuki Honda <hakobera@gmail.com>
2015-04-13 19:38:13 +0900

    Refactoring batch_put_test.js

--- scripts/batch_put_test.js
+++ scripts/batch_put_test.js
@@ -3,18 +3,26 @@
 var AWS = require('aws-sdk');
 var kinesis = new AWS.Kinesis({ region: process.env.AWS_REGION || 'us-east-1' });
 
-var params = { StreamName: process.env.STREAM_NAME || 'examplestream' };
-params.Records = [];
+var count = 0;
+var put = function () {
+  var params = { StreamName: process.env.STREAM_NAME || 'examplestream' };
+  params.Records = [];
 
-for (var i = 0; i < 300; ++i) {
-  var record = {
-    Data: JSON.stringify({ user_id: 'user_' + i }),
-    PartitionKey: 'key_' + i % 10
-  };
-  params.Records.push(record);
-}
+  for (var i = 0; i < 500; ++i) {
+    var record = {
+      Data: JSON.stringify({ user_id: 'user_' + i }),
+      PartitionKey: 'key_' + i % 10
+    };
+    params.Records.push(record);
+  }
 
-kinesis.putRecords(params, function (err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
+  kinesis.putRecords(params, function (err, data) {
+    if (err) return console.log(err);
+
+    console.log(data);
+    if (++count <= 100) {
+      setTimeout(put, 1000);
+    }
+  });
+};
+put();
