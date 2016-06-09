31414ad1d026efa655dffa9aa1dfad76aeb495b1
Takashi Kaneda <takashi.kaneda@gmail.com>
2016-06-09 20:32:37 +0900

    Update put_record.sh

--- scripts/put_record.sh
+++ scripts/put_record.sh
@@ -6,5 +6,5 @@ STREAM_NAME=${STREAM_NAME:-examplestream}
 echo "Put $1 to $STREAM_NAME"
 aws kinesis put-record \
   --stream-name $STREAM_NAME \
-  --data "$DATA" \
+  --data $DATA \
   --partition-key ${PARTITION_KEY:-shardId-000000000000}
